5250f5582f5e8cdc0dc6580ee83c6755868f9f42
junhaoshen <junhaoshen@junhaoshendemac-mini.local>
2015-12-30 17:08:12 +0800

    Initial Commit

---
+++ BlockDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockDemo
+//
+//  Created by junhaoshen on 15/12/30.
+//  Copyright © 2015年 junhaoshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
